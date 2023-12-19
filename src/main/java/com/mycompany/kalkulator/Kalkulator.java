d59ddd3320458d7b6f8e65d33f153974c820f672
ASUS <ASUS@LAPTOP-1JPDSC54>
2023-12-19 13:45:46 +0700

    ini adalah commit pertama

---
+++ src/main/java/com/mycompany/kalkulator/Kalkulator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.kalkulator;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Kalkulator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
